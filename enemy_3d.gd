616f4ea455e788fae894a3adf61fe1f8733baa2f
sookrajj <sookraj.j@stu.janesville.k12.wi.us>
2025-02-19 09:39:55 -0600

    hi

--- enemy_3d.gd
+++ enemy_3d.gd
@@ -20,24 +20,28 @@ func take_damage(_dmg) :
 func _physics_process(delta: float) -> void:
 	for player in get_tree().get_nodes_in_group("interact"):
 		if $AttackRange.overlaps_body(player):
-			print("what")
 			nav_agent_3d.target_position = player.global_position
 		if atk_area.overlaps_body(player):
 			anim.play("Punching")
 			player.queue_free()
+			await anim.animation_finished
 		
 	
-	var direction = (nav_agent_3d.target_position - self.global_position).normalized()
-	velocity = velocity.lerp(direction * speed, accel * delta)
+	var direction = (nav_agent_3d.target_position - self.global_position)
+	direction.y = 0
+	if direction.length() > 0.01:
+		var rot = atan2(direction.x, direction.y)
+		self.rotation.y = lerp_angle(rotation.y, rot, 5* delta)
+	velocity = velocity.lerp(direction.normalized() * speed, accel * delta)
 	if not is_on_floor():
-		velocity += get_gravity() * delta * 10
-	if direction.length() >0.1:
-		self.look_at(Vector3(nav_agent_3d.target_position.x, self.global_position.y, nav_agent_3d.target_position.z), Vector3.UP)
-	if velocity.length() <= 2 && anim.current_animation == "":
-		anim.play("ArmStretching")
-		print(anim.current_animation)
-	else:
-		anim.play("Walking")
+		velocity += get_gravity() * delta
+	#if direction.length() >0.1:
+		#self.look_at(Vector3(nav_agent_3d.target_position.x, self.global_position.y, nav_agent_3d.target_position.z), Vector3.UP)
+	if anim.current_animation != "Punching":
+		if velocity.length() <= 2 && anim.current_animation == "":
+			anim.play("ArmStretching")
+		elif anim.current_animation == "":
+			anim.play("Walking")
 	
 	move_and_slide()
 	
