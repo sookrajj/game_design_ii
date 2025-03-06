9e3ee33c0fe9c5348706537a3e0791c3057cf511
sookrajj <sookraj.j@stu.janesville.k12.wi.us>
2025-03-06 12:17:47 -0600

    hi

--- cutscene_3.gd
+++ cutscene_3.gd
@@ -1,10 +1,16 @@
 extends Node3D
 
 @onready var blueanim = $AuxScene/AnimationPlayer
+@onready var redanim = $MousePlayer/AuxScene/AnimationPlayer
+var t = "FastRun0";
+var o = "Running0"
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
+	await get_tree().create_timer(20).timeout
+	redanim.play(t)
 	await get_tree().create_timer(28).timeout
-	blueanim.play("Defeated0")
+	t = "Taunt0"
+	o = "Defeated0"
 	await get_tree().create_timer(2).timeout
 	get_tree().quit()
 	pass 
@@ -12,4 +18,5 @@ func _ready() -> void:
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta: float) -> void:
-	if blueanim.current_animation == "": blueanim.play("Running0")
+	if blueanim.current_animation == "": blueanim.play(o)
+	if redanim.current_animation == "": redanim.play(t)
